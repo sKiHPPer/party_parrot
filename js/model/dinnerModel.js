b97a19faff00e648d69abccc1beb4323388b6c46
hbjarre <hbjarre@kth.se>
2019-01-21 11:06:47 +0100

    Update dinnerModel.js

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -12,7 +12,6 @@ var DinnerModel = function() {
 	this.getNumberOfGuests = function() {
 		//TODO Lab 1
 	}
-	djwiji
 
 	//Returns the dish that is on the menu for selected type 
 	this.getSelectedDish = function(type) {
