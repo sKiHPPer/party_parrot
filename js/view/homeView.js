4425f1429ce205a6c6ddd8f7ac01cc0cd95fd261
MachiRic <matilda.richardson@gmail.com>
2019-01-22 08:22:09 +0100

    updates

---
+++ js/view/homeView.js
@@ -0,0 +1,12 @@
+var ExampleView = function (container, model) {
+	
+	
+	var numberOfGuests = container.find("#guests");
+
+	this.plusButton = container.find("#guests");
+	this.minusButton = container.find("#guests");
+
+	numberOfGuests.html("Hello World");
+	
+}
+ 
