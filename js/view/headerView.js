e76f59f32f467f396f2adac624507b87e51ce685
mrich <mrich@kth.se>
2019-01-22 13:48:36 +0100

    update

--- js/view/headerView.js
+++ js/view/headerView.js
@@ -1,5 +1,5 @@
 
-var ExampleView = function (container, model) {
+var headerView = function (container, model) {
 	
 	var header = container.find("#header");
 	header.html("<a href='#' id='header'><h1>Dinner planner</h1></a>");
