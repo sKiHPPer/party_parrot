0454057ee96ba41a24fca57dae58796ddd92e5ed
MachiRic <matilda.richardson@gmail.com>
2019-01-22 23:06:12 +0100

    Update sidebar.js

--- js/view/sidebar.js
+++ js/view/sidebar.js
@@ -4,11 +4,11 @@ var Sidebar = function (container, model) {
     var WholeMenu = model.getFullMenu();
     var TotalPrice = model.getTotalMenuPrice();
     var tha_dish='';
-    var html="<h6>MY DINNER</h6><p>People</p><p class='side_something'>Dish Name</p><p class='side_something'>Cost</p>";
+    var html="<h6>MY DINNER</h6><p>People</p><div class='input-guests'><input type='number' min='0' max='40' id='number_guests' value='3'></div><p class='side_something'>Dish Name</p><p class='side_something'>Cost</p>";
 
     for(dish in WholeMenu){
         tha_dish=WholeMenu[dish];
-        html=html+"<p>"+tha_dish.name+"</p>";
+        html=html+"<button class='button' id='btn'><p>"+tha_dish.name+"</p><p class='price'>"+tha_dish.price+"</p></button>";
     }
 
     html=html+"<p class='price'>SEK "+TotalPrice+"<p>";
