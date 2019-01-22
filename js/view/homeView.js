e7f876188aaa2d5d09dbcdc99da47142d7d2a89f
MachiRic <matilda.richardson@gmail.com>
2019-01-22 08:27:00 +0100

    varoius updates

--- js/view/homeView.js
+++ js/view/homeView.js
@@ -1,12 +1,4 @@
-var ExampleView = function (container, model) {
-	
-	
-	var numberOfGuests = container.find("#guests");
-
-	this.plusButton = container.find("#guests");
-	this.minusButton = container.find("#guests");
-
-	numberOfGuests.html("Hello World");
+var homeView = function (container, model) {
 	
 }
  
