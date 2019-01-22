d6ce2cd729452eff03927acc943e7c26332cc05b
MachiRic <matilda.richardson@gmail.com>
2019-01-22 22:31:06 +0100

    Update

--- js/view/sidebar.js
+++ js/view/sidebar.js
@@ -2,14 +2,14 @@ var Sidebar = function (container, model,type) {
 
     var menuView = container.find("#sidebar");
     if(document.getElementById('sidebar')){
-    var AllDishes = model.getAllDishes(type);
+    var AllDishes = model.getFullMenu();
     var tha_dish='';
-    var html='';
+    var html='<h6>MY DINNER</h6><p>People</p>';
 
     for(dish in AllDishes){
         tha_dish=AllDishes[dish];
         //alert("image: "+"<img src='"+images/tha_dish.image+"'></img>") 
-        alert("Hej");
+        console.log("Hej");
     }
 
     document.getElementById('sidebar').innerHTML=html;
