f94ff2aa73a869fa6095f1fa9a4f7fde27ce61d2
hbjarre <hbjarre@kth.se>
2019-01-22 13:39:38 +0100

    jgrf

---
+++ js/view/dishView.js
@@ -0,0 +1,14 @@
+
+var ExampleView = function (container, model) {
+
+    var numberOfGuests = container.find("#guests");
+    var dishes = container.find("#");
+
+	this.plusButton = container.find("#guests");
+	this.minusButton = container.find("#guests");
+
+    numberOfGuests.html("Hello World");
+    
+	
+}
+ 
