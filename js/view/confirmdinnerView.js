67365e4c82f471edf998d3165bc52576e39af9f4
mrich <mrich@kth.se>
2019-01-23 17:31:22 +0100

    sdfghjlk

--- js/view/confirmdinnerView.js
+++ js/view/confirmdinnerView.js
@@ -6,22 +6,25 @@ var ConfirmdinnerView = function (container, model, guests) {
         console.log(fullMenu);
         var tha_dish='';
 
-        var html="<div class='big_box_ingredients'><p>MY DINNER: "+guests+" PEOPLE</p>";
-        html=html+"<button class='button' id='btn'>Go back and edit dinner</button></div>";
+        //Menyn
+        var html="<div class='row text-center' id='my_menu'><p class='col-md-8'>MY DINNER: "+guests+" PEOPLE</p>";
+        html=html+"<div class='col-md-4'><button class='button' id='btn'>Go back and edit dinner</button></div></div><div class='row'>";
         //Alla rätter i menyn
         
+
         for(dish in fullMenu){
             tha_dish=fullMenu[dish];
             var dishPrice = model.getTotalDishPrice(tha_dish.id);
             //alert("image: "+"<img src='"+images/tha_dish.image+"'></img>") 
             console.log(tha_dish);
-            html=html+"<button id='image' class='btn_image'><img src='images/"+tha_dish.image+"'></img>"+"<p class='dish_name_menu'>"+tha_dish.name+"</p></button><p class='price'>"+dishPrice+" SEK</p>";
+            html=html+"<div class='col-md-2'><button id='image' class='btn_image'><img src='images/"+tha_dish.image+"'></img>"+"<p class='dish_name_menu'>"+tha_dish.name+"</p></button><p class='price'>"+dishPrice+" SEK</p></div>";
         }
         
         //Total price.
-        html=html+"<p>Total: </p><p class'price'>"+totalPrice+" SEK</p>";
+        html=html+"<div class='col-md-2'><p>Total: </p><p class'price'>"+totalPrice+" SEK</p></div></div>";
     
         document.getElementById('confirmdinnerView').innerHTML=html;
+        console.log("Hejsan hoppsan");
         }
     
 }
--- js/view/printView.js
+++ js/view/printView.js
@@ -7,15 +7,16 @@ var PrintView = function (container, model, guests) {
         var long_text="<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>"
 
 
-        var html="<div class='big_box_ingredients'><p>MY DINNER: "+guests+" PEOPLE</p>";
-        html=html+"<button class='button' id='btn'>Go back and edit dinner</button></div>";
+        //Menyn
+        var html="<div class='row text-center' id='my_dinner'><p class='col-md-8'>MY DINNER: "+guests+" PEOPLE</p>";
+        html=html+"<div class='col-md-4'><button class='button' id='btn'>Go back and edit dinner</button></div></div><div class='row'>";
         //Alla rätter i menyn
         
         for(dish in fullMenu){
             tha_dish=fullMenu[dish];
             //alert("image: "+"<img src='"+images/tha_dish.image+"'></img>") 
             console.log(tha_dish);
-            html=html+"<img src='images/"+tha_dish.image+"'></img>"+"<p class='dish_name_menu'>"+tha_dish.name+"</p>"+long_text+"<h5>PREPARATION</h5>"+long_text+long_text;
+            html=html+"<div class='row'><img class='col-md-4' src='images/"+tha_dish.image+"'></img>"+"<div class='col-md-4'><h4 class='dish_name_menu'>"+tha_dish.name+"</h4>"+long_text+"</div><div class='col-md-4'><h4>PREPARATION</h4>"+long_text+long_text+"</div></div>";
         }
     
     
