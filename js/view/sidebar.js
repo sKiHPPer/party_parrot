bb80f99d2fa65b37823cdcc3484b132d7425379f
mrich <mrich@kth.se>
2019-01-24 14:07:10 +0100

    Fixed meny not button
    <3<3<3<3<3

--- js/view/sidebar.js
+++ js/view/sidebar.js
@@ -4,8 +4,9 @@ var Sidebar = function (container, model, guests) {
     var WholeMenu = model.getFullMenu();
     var TotalPrice = model.getTotalMenuPrice();
     var tha_dish='';
-    var html="<h6>MY DINNER</h6><p class='input_guests'>People</p><div class='input_guests'><input type='number' min='0' max='40' id='number_guests' value='"+guests+"'></div><div class='side_cost'><p class='side_something1'>Dish Name</p><p class='side_something2'>Cost</p></div>";
-
+    //The "menu":
+    var html="<p class='input_guests'>People</p><div class='input_guests'><input type='number' min='0' max='40' id='number_guests' value='"+guests+"'></div><div class='side_cost'><p class='side_something1'>Dish Name</p><p class='side_something2'>Cost</p></div>";
+    
     for(dish in WholeMenu){
         tha_dish=WholeMenu[dish];
         console.log(tha_dish)
