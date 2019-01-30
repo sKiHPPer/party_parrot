5457154605fb644d72347d39a9dac8cb8b0c5e1e
mrich <mrich@kth.se>
2019-01-30 10:38:17 +0100

    Updated grid

--- controllers/sidebarController.js
+++ controllers/sidebarController.js
@@ -2,6 +2,6 @@ class SidebarController {
     constructor(container, generalController, model) {
         this.generalController = generalController;
         this.view = new Sidebar(container, model);
-        this.view.confirm_btn.addEventListener("click", () => this.generalController.showConfirmdinnerview());
+        this.view.button.addEventListener("click", () => this.generalController.showConfirmdinnerview());
     }
 }
--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ class generalController {
 	}
 
 	hide() {
-		this.container.innerHTML = "<div id='sidebar'>";
+		this.container.innerHTML = "<div id='sidebar'></div>";
 	}
 
 	showHomeview() {
