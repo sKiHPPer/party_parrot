4781863f03f4eb1aab91899f0e70ca40c00fbd78
hbjarre <hbjarre@kth.se>
2019-01-30 16:36:07 +0100

    Style and new controller

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -4,7 +4,7 @@ class DinnerModel {
 	// and selected dishes for the dinner menu
 
 	constructor() {
-		this.numberOfGuests = 0;
+		this.numberOfGuests = 3;
 		this.dishes = dishesConst; // to be replaced in lab 3
 		this.dinner_menu = [1,2,3];
 		this.long_text = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
