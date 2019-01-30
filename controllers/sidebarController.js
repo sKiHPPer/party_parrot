0aacaa87566f5f34e98196202a9e443971f8466e
hbjarre <hbjarre@kth.se>
2019-01-30 09:53:10 +0100

    DEN E KLAR NU

--- controllers/sidebarController.js
+++ controllers/sidebarController.js
@@ -1,6 +1,7 @@
 class SidebarController {
     constructor(container, generallController, model) {
         this.generallController = generalController;
-        this.view
+        this.view = new Sidebar(container, model);
+        this.view.button.addEventListener("click", () => this.generallController.showConfirmdinnerview());
     }
 }
